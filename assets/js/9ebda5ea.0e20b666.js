1e6c7e491d8934ebf8ddf193c465cd26b9bbe1cb
Aman Katiyar <98734779+Akatiyar123@users.noreply.github.com>
2025-04-29 10:09:22 +0530

    Delete assets directory

--- assets/js/9ebda5ea.0e20b666.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunknotes=self.webpackChunknotes||[]).push([[6483],{1689:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/agrichat-annam/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
