da082d64f57e0508c84ff420bdee4757308cecba
Aman Katiyar <aman362katiyar@gmail.com>
2025-08-07 10:30:52 +0530

    Deployment code pushed completely

--- agrichat-backend/app.py
+++ agrichat-backend/app.py
@@ -70,7 +70,7 @@ app = FastAPI(lifespan=lifespan)
 
 origins = [
     "https://agri-annam.vercel.app",
-    "https://14981d077be7.ngrok-free.app",
+    "https://7186aeaae6c8.ngrok-free.app",
     "https://localhost:3000",
     "https://127.0.0.1:3000",
     "http://localhost:3000",
