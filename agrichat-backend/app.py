e589436fdf16d8959b2a067899034c1862e968c9
Aman Katiyar <aman362katiyar@gmail.com>
2025-08-07 12:22:36 +0530

    Updated some changes for CORS

--- agrichat-backend/app.py
+++ agrichat-backend/app.py
@@ -90,6 +90,9 @@ app.add_middleware(
 async def add_ngrok_headers(request: Request, call_next):
     response = await call_next(request)
     response.headers["ngrok-skip-browser-warning"] = "true"
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "*"
     return response
 
 MONGO_URI = os.getenv("MONGO_URI")
